0d652d8e26f1503044077c97f2dcb583fe2f0a98
infoomics <sbx_94@hotmail.com>
2023-01-18 13:47:08 +0000

    init remove-last-number kata and test

---
+++ __tests__/remove-last-number.test.js
@@ -0,0 +1,10 @@
+const funcName = require('test-name');
+
+describe('', () => {
+    test('', () => {
+        const input = undefined;
+        const output = undefined;
+
+        expect(input).toSmth(output);
+    });
+});
