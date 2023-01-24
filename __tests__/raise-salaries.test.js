d717fcbed663827d9e698affe4dc8dc444d17f19
infoomics <sbx_94@hotmail.com>
2023-01-24 11:05:46 +0000

    reorganise tests

--- __tests__/raise-salaries.test.js
+++ __tests__/raise-salaries.test.js
@@ -1,16 +1,16 @@
 const raiseSalaries = require('../katas/raise-salaries');
 
 describe('', () => {
+    const input1 = [
+        { name: "Alice", salary: 3000 },
+        { name: "Bob", salary: 2000 },
+        { name: "Vel", salary: 4500 },
+    ];
+    const input2 = 10;
+
+    const output = raiseSalaries(input1, input2);
+
     test('no mutation of input array and objects', () => {
-        const input1 = [
-            { name: "Alice", salary: 3000 },
-            { name: "Bob", salary: 2000 },
-            { name: "Vel", salary: 4500 },
-        ];
-        const input2 = 10;
-
-        const output = raiseSalaries(input1, input2);
-
         const expected = [
             { name: "Alice", salary: 3000 },
             { name: "Bob", salary: 2000 },
@@ -19,16 +19,7 @@ describe('', () => {
         expect(input1).toEqual(expected);
     });
 
-    test('multiple employees', () => {
-        const input1 = [
-            { name: "Alice", salary: 3000 },
-            { name: "Bob", salary: 2000 },
-            { name: "Vel", salary: 4500 },
-        ];
-        const input2 = 10;
-
-        const output = raiseSalaries(input1, input2);
-
+    test('new obj returned with correct mutation', () => {
         const expected = [
             { name: "Alice", salary: 3300 },
             { name: "Bob", salary: 2200 },
