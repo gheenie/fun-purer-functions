2c7ee760ff7e087ba429217814082f6c31388b1b
infoomics <sbx_94@hotmail.com>
2023-01-24 19:50:41 +0000

    refactored rounding method

--- katas/raise-salaries.js
+++ katas/raise-salaries.js
@@ -1,9 +1,14 @@
+function roundTo2Decimals(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+  
+
 function raiseSalaries(arr, percentage) {
     const employees = JSON.parse( JSON.stringify(arr) );
 
     employees.forEach( (employee) => {
         // Imprecise floating point values.
-        employee.salary = Math.round(employee.salary * (percentage + 100) / 100);
+        employee.salary = roundTo2Decimals(employee.salary * (percentage + 100) / 100);
     } );
 
     return employees;
